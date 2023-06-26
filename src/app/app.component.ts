8e021a119a328a276935c83b5a634fb53088e8dd
gokul <gokulchandan18@gmail.com>
2023-06-27 00:24:04 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './Components/Header/header.component';
-import { MainComponentComponent } from './Components/Main-component/main-component.component';
 import { NavigationComponent } from './Components/Navigation/navigation.component';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, RouterModule,MainComponentComponent,NavigationComponent],
+  imports: [HeaderComponent, RouterModule,NavigationComponent],
   selector: 'angular-vite-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './Components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
