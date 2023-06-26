eaeef2e9442c70e35cc83ffbb0e22855567d4dec
gokul <gokulchandan18@gmail.com>
2023-06-27 00:27:54 +0530

    a

---
+++ src/app/Components/Income/income.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-vite-income',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './income.component.html',
+  styleUrls: ['./income.component.css'],
+})
+export class IncomeComponent {}
