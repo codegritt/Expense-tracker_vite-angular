e5c036f8f05b7baee9c815c1f94d6ad58909fb45
gokul <gokulchandan18@gmail.com>
2023-06-26 11:56:26 +0530

    a

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,27 +1,26 @@
-const router = require('express').Router();
-const { addExpense, getExpense, deleteExpense } = require('../controllers/income');
+
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expenses');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const router = require('express').Router();
 
-router.post('/add-income',(req,res)=>{
-    addIncome
-})
+router.post('/add-income',addIncome)
 
-router.get('/get-incomes',(req,res)=>{
+router.get('/get-incomes',
     getIncomes
-})
+)
 
-router.delete('/delete-income/:id',(req,res)=>{
+router.delete('/delete-income/:id',
      deleteIncome
-})
-router.post('/add-expense',(req,res)=>{
+)
+router.post('/add-expense',
     
     addExpense
-})
-router.get('/get-expenses',(req,res)=>{
+)
+router.get('/get-expenses',
      getExpense
-    })
-router.delete('/delete-expense/:id',(req,res)=>{
+)
+router.delete('/delete-expense/:id',
      deleteExpense
-    })
+  )
 
 module.exports = router
