eb259a2489d8fa5773540d8e54de204719caa555
gokul <gokulchandan18@gmail.com>
2023-06-26 13:00:32 +0530

    a

---
+++ src/app/Components/Main-component/main-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'angular-vite-main-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.css'],
+})
+export class MainComponentComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './Components/Header/header.component';
+import { MainComponentComponent } from './Components/Main-component/main-component.component';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule,MainComponentComponent],
   selector: 'angular-vite-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
