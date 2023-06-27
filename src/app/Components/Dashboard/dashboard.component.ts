da0b8dd58157bde5ed5e289090da23a9c43bea09
gokul <gokulchandan18@gmail.com>
2023-06-27 10:25:00 +0530

    a

---
+++ src/app/Components/Dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-vite-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent {}
